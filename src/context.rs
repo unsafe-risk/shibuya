20ffbcb567d3255203a8cecf960a10a3d39ba403
snowmerak <snowmerak@outlook.com>
2024-03-06 02:38:43 +0900

    rewrite context

--- src/context.rs
+++ src/context.rs
@@ -4,76 +4,92 @@ use std::any::Any;
 
 use tokio::sync::broadcast;
 
-pub enum Context<'a> {
+pub enum Context {
     Base,
-    WithData(Arc<Mutex<Context<'a>>>, HashMap<&'a str, Box<dyn Any>>),
-    WithCancel(Arc<Mutex<Context<'a>>>, broadcast::Sender<()>, bool),
+    ValueContext(Arc<Mutex<Context>>, HashMap<&'static str, &'static Box<dyn Any + 'static>>),
+    CancelContext(Arc<Mutex<Context>>, broadcast::Sender<()>, bool),
 }
 
-impl Context<'static> {
-    pub fn with_value<'x>(self, value: Vec<(&'static str, Box<dyn Any>)>) -> Arc<Mutex<Context<'static>>> {
-        let mut data = HashMap::new();
-        for (k, v) in value {
-            data.insert(k, v);
-        }
-        Arc::new(Mutex::new(Context::WithData(Arc::new(Mutex::new(self)), data)))
+pub fn background() -> Arc<Mutex<Context>> {
+    Arc::new(Mutex::new(Context::Base))
+}
+
+pub fn with_value(ctx: Arc<Mutex<Context>>, values: &'static [(&str, Box<dyn Any>)]) -> Arc<Mutex<Context>> {
+    let mut map = HashMap::new();
+    for (k, v) in values {
+        map.insert(*k, v);
     }
 
-    pub fn value<V: Any + Copy>(&'_ self, key: &'static str) -> Option<V> {
-        match self {
-            Context::Base => None,
-            Context::WithData(ctx, data) => {
-                match data.get(key) {
-                    None => {
-                        let ctx = ctx.lock().unwrap();
-                        ctx.value::<V>(key)
-                    },
-                    Some(x) => {
-                        let x = x.downcast_ref::<V>();
-                        match x {
-                            None => None,
-                            Some(x) => Some(*x),
-                        }
-                    },
-                }
-            },
-            Context::WithCancel(ctx, _, _) => {
-                match ctx.lock() {
-                    Ok(ctx) => ctx.value::<V>(key),
-                    Err(_) => None,
-                }
-            },
-        }
-    }
+    Arc::new(Mutex::new(Context::ValueContext(ctx, map)))
+}
 
-    pub fn with_cancel(self) -> (Arc<Mutex<Context<'static>>>, broadcast::Receiver<()>) {
-        let (tx, rx) = broadcast::channel(1);
-        (Arc::new(Mutex::new(Context::WithCancel(Arc::new(Mutex::new(self)), tx, false))), rx)
-    }
+pub fn with_cancel(ctx: Arc<Mutex<Context>>) -> Arc<Mutex<Context>> {
+    let (tx, _) = broadcast::channel(1);
+    Arc::new(Mutex::new(Context::CancelContext(ctx, tx, false)))
+}
 
-    pub fn cancel(&self) {
-        match self {
-            Context::Base => {},
-            Context::WithData(ctx, _) => {
-                ctx.lock().unwrap().cancel();
-            },
-            Context::WithCancel(_, tx, _) => {
-                tx.send(()).unwrap();
-            },
-        }
+pub fn value(ctx: Arc<Mutex<Context>>, key: &'static str) -> Option<&'static Box<dyn Any + 'static>> {
+    match &*ctx.lock().unwrap() {
+        Context::Base => None,
+        Context::ValueContext(ctx, map) => {
+            match map.get(key) {
+                Some(v) => Some(v),
+                None => value(ctx.clone(), key),
+            }
+        },
+        Context::CancelContext(ctx, _, _) => {
+            value(ctx.clone(), key)
+        },
     }
+}
 
-    pub fn done(&self) -> Option<broadcast::Receiver<()>> {
-        match self {
-            Context::Base => {
-                None
-            },
-            Context::WithData(ctx, _) => {
-                ctx.lock().unwrap().done()
-            },
-            Context::WithCancel(_, tx, _) => {
-                Some(tx.subscribe())
-            },
-        }
+pub fn cancel_ctx(ctx: Arc<Mutex<Context>>) {
+    match &*ctx.lock().unwrap() {
+        Context::Base => {},
+        Context::ValueContext(ctx, _) => {
+            cancel_ctx(ctx.clone());
+        },
+        Context::CancelContext(_, tx, _) => {
+            tx.send(()).unwrap();
+        },
+    }
+}
+
+pub fn done_of(ctx: Arc<Mutex<Context>>) -> Option<broadcast::Receiver<()>> {
+    match &*ctx.lock().unwrap() {
+        Context::Base => None,
+        Context::ValueContext(ctx, _) => {
+            done_of(ctx.clone())
+        },
+        Context::CancelContext(_, tx, _) => {
+            Some(tx.subscribe())
+        },
+    }
+}
+
+pub fn is_cancelled(ctx: Arc<Mutex<Context>>) -> bool {
+    match &*ctx.lock().unwrap() {
+        Context::Base => false,
+        Context::ValueContext(ctx, _) => {
+            is_cancelled(ctx.clone())
+        },
+        Context::CancelContext(_, _, done) => {
+            *done
+        },
+    }
+}
+
+pub fn after(ctx: Arc<Mutex<Context>>, fun: Arc<Mutex<dyn Fn() + Send + Sync>>) -> Result<(), &'static str> {
+    let rx = done_of(ctx.clone());
+
+    match rx {
+        Some(mut rx) => {
+            tokio::spawn(async move {
+                rx.recv().await.unwrap();
+                fun.as_ref().lock().unwrap()();
+            });
+            Ok(())
+        },
+        None => Err("Context has no done channel"),
     }
 }
