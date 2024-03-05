fda5d1b03e5430a174c3cf9d31b6361ba8e8ce85
snowmerak <snowmerak@outlook.com>
2024-03-06 02:48:25 +0900

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,22 @@
-use shibuya::context::Context;
+use std::vec;
 
+use shibuya::context::{after_ctx, background, cancel_ctx, with_cancel};
 
 #[tokio::main]
 async fn main() {
-    let ctx = Context::Base;
-    let ctx = ctx.with_value(vec![("aaaa", Box::new(123)), ("bbbb", Box::new("hello")), ("cccc", Box::new(3.14))]);
-    println!("{:?}", ctx.lock().unwrap().value::<i32>("aaaa"));
-    println!("{:?}", ctx.lock().unwrap().value::<&str>("bbbb"));
-    println!("{:?}", ctx.lock().unwrap().value::<f64>("cccc"));
+    let ctx = background();
+    let ctx = with_cancel(ctx);
+    match after_ctx(ctx.clone(), vec![Box::new(|| {
+        println!("Hello, world!");
+    })]) {
+        Ok(_) => {},
+        Err(e) => {
+            println!("{}", e);
+            return;
+        },
+    }
+
+    cancel_ctx(ctx.clone());
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 }
